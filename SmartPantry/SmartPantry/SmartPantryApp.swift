1211b6d8d919dd0f2ed49d864b7c9de1a7e60904
Long Lam <lamhoanglong.3001@gmail.com>
2024-03-04 17:16:27 -0500

    feat: init swift app

---
+++ SmartPantry/SmartPantry/SmartPantryApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartPantryApp.swift
+//  SmartPantry
+//
+//  Created by Long Lam on 3/4/24.
+//
+
+import SwiftUI
+
+@main
+struct SmartPantryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
